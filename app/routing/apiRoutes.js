b32a364ebca414764d5dd9dec940f47202134bf2
Robert <35673753+smitty10010@users.noreply.github.com>
2018-08-19 20:01:39 -0600

    added logic to pick best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,26 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-
+        var userDataArray = req.body.scores;
+        var compareValue = 200;
+        var pickedFriendName;
+        var pickedFriendPhoto;
+        for (i = 0; i < friendsData.length; i++) {
+            var compareArray = friendsData[i].scores;
+            var diff = 0;
+            for (n = 0; n < userDataArray.length; n++) {
+                var numA = userDataArray[n];
+                var numB = compareArray[n];
+                diff = diff + Math.abs(numA - numB);
+            }
+            if (diff < compareValue) {
+                compareValue = diff;
+                pickedFriendName = friendsData[i].name;
+                pickedFriendPhoto = friendsData[i].photo;
+            }
+        }
+        friendsData.push(req.body);
+        res.send({ status: 'OK', name: pickedFriendName, photo: pickedFriendPhoto });
     });
 
 };
